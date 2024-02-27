dd63687cd426173a401ad316417fe703270d2fed
edwinp <edwinpangda@gmail.com>
2024-02-27 13:44:28 +0700

    fix: referenceutils fix

--- src/Utils/FieldReferenceUtils.js
+++ src/Utils/FieldReferenceUtils.js
@@ -1,10 +1,12 @@
-import { getReference } from '../Services/AutoLayoutService'
+import { getReference } from "../Services/AutoLayoutService"
 
 export const getFieldByFieldId = (fieldId, panelList) => {
   let resultField = {}
   panelList.forEach((panelItem) => {
     // prettier-ignore
-    let findField = panelItem.listField.find((fieldItem) => fieldItem.id === fieldId)
+    let findField = panelItem.listField?.data?.find(
+      (fieldItem) => fieldItem.id === fieldId,
+    )
     if (findField) resultField = findField
   })
   return resultField
@@ -14,7 +16,7 @@ export const getChildValueByChildParent = (
   childId,
   panelList,
   getValues,
-  setValue
+  setValue,
 ) => {
   let emptyField = false
   const payload = { referenceName: childId, param: [] }
@@ -24,7 +26,7 @@ export const getChildValueByChildParent = (
   childField.reference.parent.forEach((parentId) => {
     const parentValue = getValues(parentId)
     payload.param.push({ id: parentId, value: parentValue })
-    if (!parentValue || parentValue === '') {
+    if (!parentValue || parentValue === "") {
       emptyField = true
     }
   })
@@ -32,14 +34,14 @@ export const getChildValueByChildParent = (
   if (!emptyField) {
     // get child value by payload
     getReference(payload).then((res) => {
-      if (res.data.status != '1') {
-        return window.Swal.fire('', res.data.message, 'error')
+      if (res.data.status != "1") {
+        return window.Swal.fire("", res.data.message, "error")
       }
       // set response value to child
       if (res.data.data.length) {
         setValue(childId, res.data.data[0].value, { shouldValidate: true })
         const childEl = document.getElementById(childId)
-        childEl.dispatchEvent(new Event('change'))
+        childEl.dispatchEvent(new Event("change"))
       }
     })
   }
